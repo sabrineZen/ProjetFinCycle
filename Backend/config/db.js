ad939d864320f1e5d5c044c02a45d8dd799acf73
SABRINE ZENATI <sabrine.zenati@se.univ-bejaia.dz>
2026-05-12 20:12:10 +0100

    prod

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,15 @@
-const {Sequelize}=require("sequelize");
+const { Sequelize } = require("sequelize");
 require("dotenv").config();
-const sequelize=new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,{
-        host:process.env.DB_HOST,
-        dialect:"mysql",
-  
-}
-)
-module.exports=sequelize;
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+module.exports = sequelize;
